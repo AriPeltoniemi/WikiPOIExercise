1d5994193cdc5551f84995a3a835f0d1a3a5a310
Ari Peltoniemi <ari.peltoniemi@outlook.com>
2019-09-30 12:26:15 +0300

    Initial Commit

---
+++ WikiPOIExercise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WikiPOIExercise
+//
+//  Created by Ari Peltoniemi on 30.9.2019.
+//  Copyright © 2019 Ari Peltoniemi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
