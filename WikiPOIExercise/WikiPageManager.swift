738090eb2014df0a4f83c3cb8af1a114176b6122
Ari Peltoniemi <ari.peltoniemi@outlook.com>
2019-10-03 12:40:08 +0300

    Code to fetch page info from wiki

---
+++ WikiPOIExercise/WikiPageManager.swift
@@ -0,0 +1,21 @@
+//
+//  WikiPageManager.swift
+//  WikiPOIExercise
+//
+//  Created by Ari Peltoniemi on 3.10.2019.
+//  Copyright © 2019 Ari Peltoniemi. All rights reserved.
+//
+
+import SwiftUI
+
+struct WikiPageManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WikiPageManager_Previews: PreviewProvider {
+    static var previews: some View {
+        WikiPageManager()
+    }
+}
