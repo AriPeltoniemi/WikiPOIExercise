80e6da9c78a100f66797a8e75c565e40224fdd27
Ari Peltoniemi <ari.peltoniemi@outlook.com>
2019-10-06 19:22:26 +0300

    View for wiki images

---
+++ WikiPOIExercise/WikImageIView.swift
@@ -0,0 +1,21 @@
+//
+//  WikImageIView.swift
+//  WikiPOIExercise
+//
+//  Created by Ari Peltoniemi on 6.10.2019.
+//  Copyright © 2019 Ari Peltoniemi. All rights reserved.
+//
+
+import SwiftUI
+
+struct WikImageIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WikImageIView_Previews: PreviewProvider {
+    static var previews: some View {
+        WikImageIView()
+    }
+}
